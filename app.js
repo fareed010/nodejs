4e66e7a4818aa378d184726936ebd8fc8815c251
40sherrin <fareedaskari42@gmail.com>
2021-03-25 22:24:19 +1100

    Express configuration

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.static('public'));
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.status(200).send('Welcome to our new app');
+    res.status(200).render('home-guest');
 })
 
 app.listen(port, () => console.log(`Our app is running on http://localhost:${port}`))
