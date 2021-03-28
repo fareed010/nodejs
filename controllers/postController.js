5cba7c161fedb8fdb0f2cea22c0b8af8a4cb08f4
40sherrin <fareedaskari42@gmail.com>
2021-03-28 14:37:33 +1100

    author's id retrieve

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,13 @@
+const Post = require('../models/Post');
 exports.viewCreateScreen = function(req, res){
     res.render('create-post');
+}
+
+exports.create = function(req, res) {
+    let post = new Post(req.body);
+    post.create().then(() => {
+        res.send('new post created.')
+    }).catch((errors) =>{
+        res.send(errors)
+    })
 }
