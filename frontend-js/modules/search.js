d98714493d797c6f00872c857bf4513ec996b9c5
40sherrin <fareedaskari42@gmail.com>
2021-03-30 11:39:34 +1100

    Security improvement

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -28,11 +28,16 @@ export default class Search {
   // 3. Methods
   keyPressHandler() {
     let value = this.inputField.value
-
+    if(value == ""){
+      clearTimeout(this.typingWaitTimer);
+      this.hideLoaderIcon();
+      this.hideResultsArea();
+    }
     if (value != "" && value != this.previousValue) {
       clearTimeout(this.typingWaitTimer)
-      this.showLoaderIcon()
-      this.typingWaitTimer = setTimeout(() => this.sendRequest(), 3000)
+      this.showLoaderIcon();
+      this.hideResultsArea();
+      this.typingWaitTimer = setTimeout(() => this.sendRequest(), 750)
     }
 
     this.previousValue = value
@@ -50,24 +55,16 @@ export default class Search {
   renderResultsHTML(posts){
     if (posts.length){
       this.resultsArea.innerHTML = `<div class="list-group shadow-sm">
-      <div class="list-group-item active"><strong>Search Results</strong> (4 items found)</div>
-
-      <a href="#" class="list-group-item list-group-item-action">
-        <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #1</strong>
-        <span class="text-muted small">by barksalot on 0/14/2019</span>
-      </a>
-      <a href="#" class="list-group-item list-group-item-action">
-        <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #2</strong>
-        <span class="text-muted small">by brad on 0/12/2019</span>
-      </a>
-      <a href="#" class="list-group-item list-group-item-action">
-        <img class="avatar-tiny" src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"> <strong>Example Post #3</strong>
-        <span class="text-muted small">by barksalot on 0/14/2019</span>
-      </a>
-      <a href="#" class="list-group-item list-group-item-action">
-        <img class="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"> <strong>Example Post #4</strong>
-        <span class="text-muted small">by brad on 0/12/2019</span>
-      </a>
+      <div class="list-group-item active"><strong>Search Results</strong> (${posts.length > 1 ? `${posts.length} items found` : '1 item found'})</div>
+        ${posts.map((post) => {
+          let postDate = new Date(post.createdDate);
+          return `
+          <a href="/post/${post._id}" class="list-group-item list-group-item-action">
+            <img class="avatar-tiny" src="${post.author.avatar}"> <strong>${post.title}</strong>
+            <span class="text-muted small">by ${post.author.username} on ${postDate.getMonth() + 1}/${postDate.getDate()}/${postDate.getFullYear()}</span>
+          </a>
+          `
+        }).join("")}
     </div>`
     } else {
       this.resultsArea.innerHTML = `<p class="alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search.</p>`
@@ -84,6 +81,9 @@ export default class Search {
   }
   showResultsArea() {
     this.resultsArea.classList.add('live-search-results--visible');
+  }
+  hideResultsArea() {
+    this.resultsArea.classList.remove('live-search-results--visible');
   }
 
   openOverlay() {
